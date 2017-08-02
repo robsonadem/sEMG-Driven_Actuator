54a1e340f6a1623bf9414dcd5e506df80d117fa7
robsonadem <ademr@lafayette.edu>
2017-08-02 12:11:25 -0700

    Add files via upload

---
+++ sEMG_Processing_Logic.ino
@@ -0,0 +1,30 @@
+
+int sensorValueThreshold = 400;
+int LED=5;
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+  pinMode(LED, OUTPUT);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // read the input on analog pin 0:
+  int sensorValue = analogRead(A0);
+  Serial.print(sensorValue);
+  if (sensorValue > sensorValueThreshold){
+    Serial.println("---CONTRACTION DETECTED");
+    digitalWrite(LED, HIGH);
+  }
+  else{
+    Serial.println("");
+    digitalWrite(LED, LOW);
+  }
+  delay(100);
+}
+
+
+
+
+
